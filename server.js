20305e71235403b3e2a5481c1af0e73d885cc646
front_Dasha <64001891+Mango-Tango19@users.noreply.github.com>
2022-11-08 12:30:06 +0300

    add error handler anr cors middleware

--- server.js
+++ server.js
@@ -2,12 +2,19 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const { logger } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const PORT = process.env.PORT || 3500;
 
 app.use(logger);
 
+app.use(cors());
+
 //ability to recieve and send json
 app.use(express.json());
+
+app.use(cookieParser());
 
 //build in middleware
 app.use("/", express.static(path.join(__dirname, "public")));
@@ -23,6 +30,8 @@ app.all("*", (req, res) => {
 		res.type("txt").send("404 Not found");
 	}
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
 	console.log(`Server running at port ${PORT}`);
