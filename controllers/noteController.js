98a8bfd85992de34ef137e007cce72fc89f05981
front_Dasha <64001891+Mango-Tango19@users.noreply.github.com>
2022-11-28 11:03:01 +0300

    add delete note controller

--- controllers/noteController.js
+++ controllers/noteController.js
@@ -26,7 +26,6 @@ const createNewNote = asyncHandler(async (req, res) => {
 	}
 
 	//check for duplicates
-
 	const duplicate = await Note.findOne({ title }).lean().exec();
 
 	if (!duplicate) {
@@ -47,9 +46,9 @@ const createNewNote = asyncHandler(async (req, res) => {
 });
 
 const updateNote = asyncHandler(async (req, res) => {
-	const { userId, title, text, completed, id } = req.body;
+	const { user, title, text, id, completed } = req.body;
 
-	if (!userId || !title || !text) {
+	if (!user || !title || !text || !id || typeof completed !== "boolean") {
 		res.status(400).json({ message: "User, title and text are required" });
 	}
 
@@ -66,9 +65,34 @@ const updateNote = asyncHandler(async (req, res) => {
 			message: "Duplicate note, two notes with same title",
 		});
 	}
+
+	note.user = user;
+	note.title = title;
+	note.text = text;
+	note.completed = completed;
+
+	const updateNote = await note.save();
+
+	res.json(`Note '${title}' updated`);
 });
 
-const deleteNote = asyncHandler(async (req, res) => {});
+const deleteNote = asyncHandler(async (req, res) => {
+	const { id } = req.body;
+
+	if (!id) {
+		res.status(400).json({ message: "User id is required" });
+	}
+
+	const note = await findOne({ id }).lean().exec();
+
+	if (!note) {
+		res.status(400).json({ message: "Note wasnt found" });
+	}
+
+	const result = await note.deleteNote();
+
+	res.json({ message: `Note ${result.title} was deleted` });
+});
 
 module.exports = {
 	getAllNotes,
